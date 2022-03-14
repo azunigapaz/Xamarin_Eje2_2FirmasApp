39dde501e38a010c32b97ad221a6cd551fba585e
Alejandro <azunigapaz@gmail.com>
2022-03-13 23:11:59 -0500

    Commit Xamarin Firmas App

---
+++ Ejercicio2_2FirmasApp/Ejercicio2_2FirmasApp/Views/PageAddFirma.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.IO;
+using SignaturePad.Forms;
+using Ejercicio2_2FirmasApp.Models;
+
+namespace Ejercicio2_2FirmasApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAddFirma : ContentPage
+    {
+        public PageAddFirma()
+        {
+            InitializeComponent();
+        }
+
+        byte[] ImageBytes;
+
+        private async void btnAgregar_Clicked(object sender, EventArgs e)
+        {
+            Stream Image = await FirmaPad.GetImageStreamAsync(SignatureImageFormat.Png);
+
+            try
+            {
+                //convertimos imagen a base 64
+                var image = await FirmaPad.GetImageStreamAsync(SignaturePad.Forms.SignatureImageFormat.Png);
+                var mStream = (MemoryStream)image;
+                byte[] data = mStream.ToArray();
+                string base64Val = Convert.ToBase64String(data);
+                ImageBytes = Convert.FromBase64String(base64Val);
+
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+
+
+            var firma = new FirmasAppModel
+            {
+                FirmaNombre = txtNombre.Text,
+                FirmaDescripcion = txtDescripcion.Text,
+                FirmaImagen = ImageBytes
+            };
+
+
+            //var resultado = await App.BaseDatos.GuardarSitio(sitio);
+
+            if (String.IsNullOrEmpty(txtNombre.Text) || String.IsNullOrEmpty(txtDescripcion.Text))
+            {
+                await DisplayAlert("Aviso", "Favor no dejar campos vacios", "Ok");
+
+            }
+            else
+            {
+                try
+                {
+                    await DisplayAlert("Aviso", "Firma Registrada con Exito!!!", "Ok");
+                    await App.FirmasAppDB.GuardarFirma(firma);
+                    FirmaPad.Clear();
+                    txtNombre.Text = "";
+                    txtDescripcion.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Advertencia", ex.Message + " Tabla Firmas Creada !", "Ok");
+                }
+
+            }
+
+            await Navigation.PopAsync();
+
+        }
+
+        private async void btnListar_Clicked(object sender, EventArgs e)
+        {
+            var consultaFirmas = new Views.PageConsultaFirmas();
+            await Navigation.PushAsync(consultaFirmas);
+        }
+
+        private void btnSalir_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
---
+++ Ejercicio2_2FirmasApp/Ejercicio2_2FirmasApp/Views/PageConsultaFirmas.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using Ejercicio2_2FirmasApp.Models;
+
+namespace Ejercicio2_2FirmasApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageConsultaFirmas : ContentPage
+    {
+        FirmasAppModel firmasModel = new FirmasAppModel();
+        public PageConsultaFirmas()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var listaFirmas = await App.FirmasAppDB.ListaFirmas();
+            ObservableCollection<FirmasAppModel> observableCollectionFirmas = new ObservableCollection<FirmasAppModel>();
+            listViewConsultaFirmas.ItemsSource = observableCollectionFirmas;
+            foreach (FirmasAppModel FirmaImagen in listaFirmas)
+            {
+                observableCollectionFirmas.Add(FirmaImagen);
+            }
+        }
+
+        private async void listViewConsultaFirmas_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                FirmasAppModel modelItemLista = (FirmasAppModel)e.Item;
+
+                var messageAlert = await DisplayAlert("Opción", "Seleccione una opción", "Editar Firma", "Eliminar Firma");
+
+                if (messageAlert)
+                {                    
+                    Models.FirmasAppModel item = (Models.FirmasAppModel)e.Item;
+                    var newpage = new Views.PageActualizarFirma();
+                    newpage.BindingContext = item;
+                    await Navigation.PushAsync(newpage);
+                }
+                else
+                {
+                    var resultadoDelete = await App.FirmasAppDB.EliminarFirma(modelItemLista);
+
+                    if (resultadoDelete != 0)
+                    {
+                        await DisplayAlert("Aviso", "Firma eliminado !", "Ok");
+                    }
+                    else
+                    {
+                        await DisplayAlert("Aviso", "Ha ocurrido un error !", "Ok");
+                    }
+
+                    obtenerListaFirmas();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Aviso", ex.Message, "Ok");
+            }
+        }
+
+
+        private async void obtenerListaFirmas()
+        {
+            var listaFirmas = await App.FirmasAppDB.ListaFirmas();
+            //Creamos un colleccion observable para que los cambios que se realizan en el modelo se reflejen de manera automatica en el View
+            ObservableCollection<FirmasAppModel> observableCollectionFotos = new ObservableCollection<FirmasAppModel>();
+            listViewConsultaFirmas.ItemsSource = observableCollectionFotos;
+            foreach (FirmasAppModel FirmaImagen in listaFirmas)
+            {
+                observableCollectionFotos.Add(FirmaImagen);
+            }
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
